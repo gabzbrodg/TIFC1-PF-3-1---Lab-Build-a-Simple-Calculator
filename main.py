123e9ca0891f0bb5d6af3e25e6de7e245b7acbe9
gabzbrodg <ibrham496@gmail.com>
2026-02-06 18:11:33 -0500

    Ultima modificación todo quedo OK

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ number2 = int(input("Ingresa el numero: "))
 
 result = number1 + number2
 print(result) 
+
+def calculadora_pro():
+    op = input("Elige: (+, -, *, /, %, triple, mixta): ")
+    
+    if op == "triple":
+        a = float(input("Num 1: ")); b = float(input("Num 2: ")); c = float(input("Num 3: "))
+        print(f"Resultado: {a + b + c}")
+    elif op == "mixta":
+        expr = input("Escribe la operación (ej. 2 + 4 - 3): ")
+        print(f"Resultado: {eval(expr)}") # eval procesa la cadena matemática
+    else:
+        n1 = float(input("Num 1: "))
+        n2 = float(input("Num 2: "))
+        if op == "-": print(n1 - n2)
+        elif op == "*": print(n1 * n2)
+        elif op == "/": print(n1 / n2) if n2 != 0 else print("Error: Div por 0")
+        elif op == "%": print(n1 % n2)
